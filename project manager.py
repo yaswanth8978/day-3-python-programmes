b9565e2d15ed5392b55df27dee2b2bac42014acf
yaswanth8978 <115204301+yaswanth8978@users.noreply.github.com>
2022-10-07 09:58:21 +0530

    Create project manager.py

---
+++ project manager.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+d=[]
+d.append(a)
+d.append(b)
+d.append(c)
+for i in range(0,3):
+    for j in range(0,3):
+        for k in range(0,3):
+            if(i!=j&j!=k&k!=i):
+                print(d[i],d[j],d[k])
